6d38430d47e38ebeb90c7697199bc49983b1e6e3
NeelaShruthi2005 <neela.shruthi@ibm.com>
2025-12-22 21:06:10 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4/Activity4Demo2.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Activity4Demo2 {
+	 @Test
+	    public void TestCase() {
+	        System.out.println("I'm in the test case from DemoTwo Class");
+	    }
+}
